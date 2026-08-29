fc43a05ba9b289c2804d519abb04687e19ff0eee
baraunab <bbarauna.beatriz@gmail.com>
2026-08-28 21:14:33 -0400

    finalizacao da pratica de js

--- dweb/aula-01/script/script.js
+++ dweb/aula-01/script/script.js
@@ -12,4 +12,28 @@ function alternarTema() {
     }
 }
 
-botaoTema.addEventListener("click", alternarTema);
+
+// botaoTema.addEventListener("click", alternarTema);
+
+/* funcao anonima (sem nome)
+botaoTema.addEventListener("click", function () {
+    const estaNoModoEscuro = corpo.classList.toggle('dark-mode');
+    
+    if (estaNoModoEscuro) {
+        botaoTema.textContent = "Modo Claro";
+    } else {
+        botaoTema.textContent = "Modo Escuro";
+    }
+});
+*/
+
+// arrow function => 
+botaoTema.addEventListener("click", () => {
+    const estaNoModoEscuro = corpo.classList.toggle('dark-mode');
+    
+    if (estaNoModoEscuro) {
+        botaoTema.textContent = "Modo Claro";
+    } else {
+        botaoTema.textContent = "Modo Escuro";
+    }
+});
