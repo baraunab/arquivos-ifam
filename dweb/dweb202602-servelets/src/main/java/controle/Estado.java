c4956ee937739d87199d6eb93d861aea4f3806df
baraunab <bbarauna.beatriz@gmail.com>
2026-09-04 19:02:45 -0400

    Adicao das classes estado e cidade

---
+++ dweb/dweb202602-servelets/src/main/java/controle/Estado.java
@@ -0,0 +1,25 @@
+package controle;
+
+public class Estado {
+    private String nome;
+    private String sigla;
+
+
+    // pode acessar o atributo
+    public String getNome () {
+        return nome;
+    }
+    // pode escrever no atributo
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    // pode acessar o atributo
+    public String getSigla() {
+        return sigla;
+    }
+    // pode escrever no atributo
+    public void setSigla(String sigla) {
+        this.nome = sigla;
+    }
+
+}
---
+++ dweb/dweb202602-servelets/src/main/java/model/Cidade.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Cidade {
+
+    private String nome;
+
+    // pode acessar o atributo
+    public String getNome () {
+        return nome;
+    }
+    // pode escrever no atributo
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
