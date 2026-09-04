7b102f9a4adb701af02e5815cb4c1d57c8b84413
baraunab <bbarauna.beatriz@gmail.com>
2026-09-04 19:10:05 -0400

    finalizacao da atividade

--- dweb/dweb202602-servelets/src/main/java/model/Cidade.java
+++ dweb/dweb202602-servelets/src/main/java/model/Cidade.java
@@ -1,16 +1,23 @@
 package model;
 
+import controle.Estado;
+
 public class Cidade {
 
     private String nome;
+    private Estado estado;
 
-    // pode acessar o atributo
     public String getNome () {
         return nome;
     }
-    // pode escrever no atributo
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public Estado getEstado () {
+        return estado;
+    }
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
