d1c7303b9ed22c3d669e3e2017b958bcb43660f2
baraunab <bbarauna.beatriz@gmail.com>
2026-09-04 22:19:24 -0400

    Servlets, criacao de pagina dinamica com insercao de dados em java no html!!!

--- dweb/dweb202602-servelets/src/main/java/servlet/OlaIfamServlet.java
+++ dweb/dweb202602-servelets/src/main/java/servlet/OlaIfamServlet.java
@@ -4,6 +4,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Cidade;
+import model.Estado;
+import model.Pessoa;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +16,21 @@ public class OlaIfamServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Pessoa pessoa = new Pessoa();
+        Estado estado = new Estado();
+        Cidade cidade = new Cidade();
+
+        estado.setNome("Amazonas");
+        estado.setSigla("AM");
+
+        cidade.setNome("Manaus");
+        cidade.setEstado(estado);
+
+        pessoa.setNome("Beatriz");
+        pessoa.setEmail("beatriz@gmail.com");
+        pessoa.setTelefone("4002-8922");
+        pessoa.setCidade(cidade);
 
         PrintWriter saida = resp.getWriter();
 
@@ -24,6 +42,11 @@ public class OlaIfamServlet extends HttpServlet {
         saida.println("</head>");
         saida.println("<body>");
         saida.println("<h1>Olá IFAM!</h1>");
+        saida.println("<p>Nome: " + pessoa.getNome() + " </p>");
+        saida.println("<p>Email: " + pessoa.getEmail() + " </p>");
+        saida.println("<p>Telefone: " + pessoa.getTelefone() + " </p>");
+        saida.println("<p>Cidade: " + pessoa.getCidade().getNome() + " </p>");
+        saida.println("<p>Estado (UF): " + pessoa.getCidade().getEstado().getNome() + " (" + pessoa.getCidade().getEstado().getSigla() + ")  </p>");
         saida.println("</body>");
         saida.println("</html>");
     }
