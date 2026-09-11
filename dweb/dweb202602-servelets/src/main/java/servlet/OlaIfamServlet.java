fd2af138e83574cef1a814f22b3623008a19e1b5
baraunab <bbarauna.beatriz@gmail.com>
2026-09-11 19:03:02 -0400

    adicao da atividade

--- dweb/dweb202602-servelets/src/main/java/servlet/OlaIfamServlet.java
+++ dweb/dweb202602-servelets/src/main/java/servlet/OlaIfamServlet.java
@@ -40,7 +40,6 @@ public class OlaIfamServlet extends HttpServlet {
         saida.println("     <meta charsert=\"UTF-8\">");
         saida.println("     <title>Ola ifam</title>");
         saida.println("</head>");
-
         saida.println("<body>");
         saida.println("     <h1>Olá IFAM!</h1>");
         saida.println("     <p><b>Nome</b>: " + pessoa.getNome() + " </p>");
@@ -48,6 +47,26 @@ public class OlaIfamServlet extends HttpServlet {
         saida.println("     <p><b>Telefone</b>: " + pessoa.getTelefone() + " </p>");
         saida.println("     <p><b>Cidade</b>: " + pessoa.getCidade().getNome() + " </p>");
         saida.println("     <p><b>Estado (UF)</b>: " + pessoa.getCidade().getEstado().getNome() + " <b>(" + pessoa.getCidade().getEstado().getSigla() + ")</b>  </p>");
+        saida.println("     <table border=1px>");
+        saida.println("         <thead>");
+        saida.println("             <tr>");
+        saida.println("                 <th>Nome</th>");
+        saida.println("                 <th>Email</th>");
+        saida.println("                 <th>Telefone</th>");
+        saida.println("                 <th>Cidade</th>");
+        saida.println("                 <th>Estado</th>");
+        saida.println("             </tr>");
+        saida.println("         </thead>");
+        saida.println("         <tbody>");
+        saida.println("             <tr>");
+        saida.println("                 <td>" + pessoa.getNome() + "</td>");
+        saida.println("                 <td>" + pessoa.getEmail() + "</td>");
+        saida.println("                 <td>" + pessoa.getTelefone() + "</td>");
+        saida.println("                 <td>" + pessoa.getCidade().getNome() + "</td>");
+        saida.println("                 <td>" + pessoa.getCidade().getEstado().getNome() + "</td>");
+        saida.println("             <tr>");
+        saida.println("         </tbody>");
+        saida.println("     </table>");
         saida.println("</body>");
 
         saida.println("</html>");
